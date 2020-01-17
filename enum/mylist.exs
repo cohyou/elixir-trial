954ac30b2c0799b6f70aa2f97e958c715394321a
OKAZAKI Yasuo <cohyou@ironoir.io>
2020-01-17 21:42:57 +0900

    making each

--- enum/mylist.exs
+++ enum/mylist.exs
@@ -1,5 +1,18 @@
 defmodule MyList do
-  def all?(enumerable, pred) do
+  def all?(enumerable, pred) do    
+    _all? Enum.to_list(enumerable), pred, true
+  end
+  defp _all?([], _, acc) do
+    acc
+  end
+  defp _all?([head|tail], pred, true) do
+    _all? tail, pred, pred.(head)
+  end
+  defp _all?(_, _, false) do
+    false
+  end
+
+  def each(enumerable, pred) do
     
-  end 
+  end
 end
